fab7fe65f56cdfb2333a0d82515be1ea168fcd8b
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-06-07 13:09:22 +0200

    a

--- include/philip/philip.hpp
+++ include/philip/philip.hpp
@@ -1,2 +1,11 @@
 #pragma once
+#include <ph_concepts/concepts.hpp>
+#include "common.hpp"
 
+constexpr auto mod (Integer auto x, Integer auto y) -> Integer auto;
+
+auto mod (Floating auto x, Floating auto y) -> Floating auto;
+
+
+
+auto for_each (Range auto or )
--- src/philip.cpp
+++ src/philip.cpp
@@ -1,32 +1,15 @@
-//#include "algorithms.hpp"
-//#include <ph_color/color.hpp>
-//
-//
-//auto debug (auto const& A, auto const& low, auto const& mid, auto const& high)
-//{
-////    cout << low << " : " << mid << " : " << high << endl;
-//    std::cout << ph::color::white;
-//    
-//    for (int i = 0; i < low; ++i)
-//        cout << A [i] << " ";
-//    
-//    
-//    
-//    std::cout << ph::color::red << A [low] << " " << ph::color::white;
-//    
-//    for (auto i = A.begin () + low + 1; i < A.begin () + mid; ++i)
-//        cout << *i << " ";
-//    
-//    std::cout << ph::color::green << A [mid] << " " << ph::color::white;
-//    
-//    for (auto i = mid + 1; i < high - 1; ++i)
-//        cout << A [i] << " ";
-//    
-//    std::cout << ph::color::yellow << A [high-1] << " " << ph::color::white;
-//    
-//    for (auto i = high; i < A.size (); ++i)
-//        cout << A [i] << " ";
-//    
-//    
-//    cout << endl;
-//}
+#include "philip.hpp"
+
+constexpr auto mod (Integer auto x, Integer auto y) -> Integer auto
+{
+    return x % y;
+}
+
+
+static_assert (mod (4, 3) == 1, "");
+
+
+auto mod (Floating auto x, Floating auto y) -> Floating auto
+{
+    return std::modf (x, y);
+}
